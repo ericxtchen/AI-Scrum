60693a6f820a1d6c0fe7c24ad93b3ac0d5b249f0
ericxtchen <96218207+ericxtchen@users.noreply.github.com>
2025-08-07 00:48:33 -0400

    Initial commit

---
+++ src/main/java/com/ericxtchen/aiscrum/AiScrumApplication.java
@@ -0,0 +1,13 @@
+package com.ericxtchen.aiscrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiScrumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiScrumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ericxtchen/aiscrum/AiScrumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericxtchen.aiscrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiScrumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
